989bfc46493bffb7fc95910bb9978c9405997068
Mike Peditto <Mike@MikePeditto.com>
2025-06-16 10:47:30 -0500

    Add logging to debug UPLOADTHING_SECRET in preview

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,12 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "@/utils/uploadthing";
 
+
+console.log("🔒 UPLOADTHING_SECRET on server:", process.env.UPLOADTHING_SECRET);
+
+
+export const runtime = "nodejs";
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
